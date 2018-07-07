00867bb52eb52ed75c430128544dcaf534440d5b
Vanessa Sochat <vsochat@stanford.edu>
2018-07-07 13:49:53 -0700

    adding farm

--- farm.cpp
+++ farm.cpp
@@ -1,7 +1,9 @@
 /* Moo moo, mothertruckers
 
     A farmer buys 100 animals for $100, How many of each animal did he buy? 
-
+    This script is probably terrible, it was my first effort to learn
+    (small things) about structs and passed by reference.
+    
     Horses cost $10, 
     pigs cost $3, and 
     rabbits are only $0.50. 
@@ -9,79 +11,100 @@
 */
 
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
-// Let's make farm animals!
-
-struct Animal
-{
-
+struct Animal {
   string name;
   double price;
   int number;
-
 } pig, rabbit, horse;
 
-struct Wallet 
-{
+struct Farm { 
+  Animal horses;
+  Animal pigs;
+  Animal rabbits;
+} farm;
+
+struct Wallet {
   double spent;    // total dollars spent
   double dollars;  // total dollars remaining
 } wallet;
 
+
 Animal makeAnimal(string name, double price);
-Wallet buyAnimals(Wallet w, Animal a);
+Farm buyAnimals(Wallet * w, Farm * f);
 Wallet getWallet(double dollars);
+Farm makeFarm(Animal horses, Animal pigs, Animal rabbits);
 
 int main() {
 
   string name;
   double price;
   double dollars = 100.0;
+  bool maxout = false;
 
   // 1. Grab your wallet!
   wallet = getWallet(dollars);
 
   // 2. Make the Animals
+  horse = makeAnimal(name = "horse", price = 10.00);
   pig = makeAnimal(name = "pig", price = 3.00);
   rabbit = makeAnimal(name = "rabbit", price = 0.50);
-  horse = makeAnimal(name = "horse", price = 10.00);
 
-  // 3. Calculate cost for each animal
-  buyAnimals(wallet, pig);
-  buyAnimals(wallet, rabbit);
-  buyAnimals(wallet, horse);
+  // 3. Put them in a farm
+  farm = makeFarm(horse, pig, rabbit);
+
+  // 4. Calculate cost for each animal
+  buyAnimals(&wallet, &farm);
+  return 0;
 
 }
 
-Animal makeAnimal(string name, float price) {
-    
-    Animal a;
-    a.name = name;
-    a.price = price;
-    return a;
+Farm makeFarm(Animal horses, Animal pigs, Animal rabbits) {
+  Farm f;
+  f.horses = horses;
+  f.pigs = pigs;
+  f.rabbits = rabbits;
+  return f;
+}
+
+Animal makeAnimal(string name, double price) {  
+  Animal a;
+  a.name = name;
+  a.price = price;
+  cout << "Created animal " << a.name << " with cost " << a.price << endl;
+  return a;
 }
 
 Wallet getWallet(double dollars) {
     
-    Wallet w;
-    w.spent = 0.0;
-    w.dollars = dollars;
-    return w;
+  Wallet w;
+  w.spent = 0.0;
+  w.dollars = dollars;
+  cout << "Created wallet with $" << w.dollars << endl;
+  return w;
 }
 
 
-Wallet buyAnimals(Wallet w, Animal a) {
+Farm buyAnimals(Wallet * w, Farm * f) {
 
-    // 1. Money spent is (integer) of dollars / price 
-    //double spent = w.dollars / a.price;
+  // Number of horses
+  for (int h = 0; h < 100; h++)
 
-    // 2. Money we have left over is the remainder
-    //a.number = w.dollars % a.price;
+    // Number of pigs
+    for (int p = 0; p < 100; p++)
 
-    //w.spent = a.number * a.price;
+      // NUmber of rabbits
+      for (int r = 0; r < 100; r++)
 
-    //cout << "You bought " << a.number << " " << a.name << "s for" << a.spent << endl; 
-    return w;
+        // Count must be == 100
+        if ((h + p + r) == 100)
 
+          // Total price must also == $100
+          if (((f->horses.price * h) + (f->pigs.price * p) + (f->rabbits.price * r)) == 100)
+            cout << "Found one! " << h << " horses " << p << " pigs " << r << " rabbits "  << endl;
+
+  return *f;
 }
